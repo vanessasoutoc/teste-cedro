0309d8dafddd15e9286a0b136db545ac1f5f25a5
vanessa <vanessasoutoc@gmail.com>
2018-07-24 10:04:44 -0300

    error route documentation cors

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,4 +1,4 @@
-GrapeSwaggerRails.options.app_url = Rails.env == "production" ? "http://teste-cedro.herokuapp.com" : "http://localhost:3000"
+GrapeSwaggerRails.options.app_url = Rails.env == "production" ? "https://teste-cedro.herokuapp.com" : "http://localhost:3000"
 
 GrapeSwaggerRails.options.app_name = 'Teste Cedro'
 GrapeSwaggerRails.options.url = '/api/v1/swagger_doc'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :disks
+    default_url_options :host => "teste-cedro.herokuapp.com"
+    resources :disks
     resources :restaurants
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
